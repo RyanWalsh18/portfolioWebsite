2374c5cd9f3b74c4c009eb0b5aa279260994eac9
Ryan Walsh <ed20rw2@leeds.ac.uk>
2024-06-11 16:31:53 +0100

    added responsivity to navbar

--- portfolio-website/src/components/Navbar.js
+++ portfolio-website/src/components/Navbar.js
@@ -1,24 +1,52 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
 import ReorderIcon from '@mui/icons-material/Reorder';
 
 function Navbar() {
+  const [openLinks, setOpenLinks] = useState(false);
+
+  const toggleNavbar = () => {
+    setOpenLinks(!openLinks);
+  };
+
+  const handleResize = () => {
+    if (window.innerWidth > 600) {
+      setOpenLinks(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    // Initial check
+    handleResize();
+    
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <div className='navbar'>
-        <div className='leftSide'>
-            <h1>Ryan Walsh</h1>
+      <div className='leftSide' id={openLinks ? "open" : "close"}>
+        <h1>Ryan Walsh</h1>
+        <div className='hiddenLinks'>
+          <Link to="/"> Home </Link>
+          <Link to="/"> Projects </Link>
+          <Link to="/"> About </Link>
         </div>
-        <div className='rightSide'>
-            <Link to="/"> Home </Link>
-            <Link to="/"> Projects </Link>
-            <Link to="/"> About Me </Link>
-            <button>
-                <ReorderIcon></ReorderIcon>
-            </button>
-        </div>
+      </div>
+      <div className='rightSide'>
+        <Link to="/"> Home </Link>
+        <Link to="/"> Projects </Link>
+        <Link to="/"> About </Link>
+        <button onClick={toggleNavbar}>
+          <ReorderIcon />
+        </button>
+      </div>
     </div>
   );
 }
 
-export default Navbar
+export default Navbar;
