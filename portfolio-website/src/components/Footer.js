db2b33e35cfe4345346aa8c53c824f3846d5e2bd
Ryan Walsh <ed20rw2@leeds.ac.uk>
2024-07-29 13:10:57 +0100

    Added linkedIn link and favicon

--- portfolio-website/src/components/Footer.js
+++ portfolio-website/src/components/Footer.js
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from 'react';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import '../styles/Footer.css';
 
 function Footer() {
   return (
     <div className='footer'>
-        <div className='socialMedia'>
-            <LinkedInIcon></LinkedInIcon>
-        </div>
+      <div className='socialMedia'>
+        <a href="https://www.linkedin.com/in/ryan-walsh-706a88256" target="_blank" rel="noopener noreferrer">
+          <LinkedInIcon />
+        </a>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Footer;
